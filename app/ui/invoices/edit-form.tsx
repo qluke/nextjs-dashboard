2ec012761d0d74b9e64961185a6ab548f7d1ae5a
luke <1156953217@qq.com>
2024-01-17 23:40:37 +0800

    improving-accessibility

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -18,6 +18,7 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
   return (
